2449f194b9851e1d1053db7d205f109b4a668c4f
salvo <salvo.bevi@gmail.com>
2024-02-02 12:44:49 +0000

    clean script.js

--- script.js
+++ script.js
@@ -39,33 +39,3 @@ function easeInOutQuad(t, b, c, d) {
 
 // Get the height of the header
 const headerHeight = document.querySelector('header').offsetHeight;
-
-// Adjust the scrolling function to account for the header height
-function animatedScrollTo(element, to, duration) {
-  var start = element.scrollTop,
-      change = to - start - headerHeight, // Subtract the header height here
-      currentTime = 0,
-      increment = 20;
-      
-  var animateScroll = function() {
-    currentTime += increment;
-    var val = easeInOutQuad(currentTime, start, change, duration);
-    element.scrollTop = val;
-    if(currentTime < duration) {
-      setTimeout(animateScroll, increment);
-    }
-  };
-  animateScroll();
-}
-
-// Apply the adjusted animated scroll to anchor clicks
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-        if (targetElement) {
-            animatedScrollTo(document.documentElement, targetElement.offsetTop, 1200); // Duration of 1200ms
-        }
-    });
-});
