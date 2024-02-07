df5d5ba73509b9218771b34b10e1900895784092
salvo <salvo.bevi@gmail.com>
2024-02-07 15:40:50 +0000

    Scroll Back To Top Button

--- script.js
+++ script.js
@@ -48,3 +48,19 @@ var menuToggle = document.getElementById('menu-toggle');
                 menuToggle.checked = false;
             });
         });
+
+// ===== Scroll to Top ==== 
+$(document).ready(function() {
+  $(window).scroll(function() {
+      if ($(this).scrollTop() >= 50) {
+          $('#return-to-top').fadeIn(200);
+      } else {
+          $('#return-to-top').fadeOut(200);
+      }
+  });
+  $('#return-to-top').click(function() {
+      $('body,html').animate({
+          scrollTop : 0
+      }, 500);
+  });
+});
